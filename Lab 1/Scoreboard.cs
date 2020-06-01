b8b5ea936f6a69afb706fe811b6e2e1fa5cd8e54
AntonNov <56130014+AntonNov@users.noreply.github.com>
2020-06-01 04:07:14 +0300

    Add files via upload

---
+++ Lab 1/Scoreboard.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_1
+{
+    class Scoreboard
+    {
+        public int countOfFirstPlayer { get; set; } = 0;
+
+        public int countOfComputer { get; set; } = 0;
+
+        public static int height { get; } = 9;
+
+        public void Draw(string temp = "??")
+        {
+
+            int i = 0;
+
+            Console.SetCursorPosition(Console.WindowWidth / 2 - 5, i);
+
+            Console.WriteLine("##########");
+
+            Console.SetCursorPosition(Console.WindowWidth / 2 - 5, ++i);
+
+            Console.WriteLine("#   YOU  #");
+
+            Console.SetCursorPosition(Console.WindowWidth / 2 - 5, ++i);
+
+            if (countOfFirstPlayer < 10)
+
+                Console.WriteLine($"#   {countOfFirstPlayer}    #");
+            else
+
+                Console.WriteLine($"#   {countOfFirstPlayer}   #");
+
+
+            Console.SetCursorPosition(Console.WindowWidth / 2 - 5, ++i);
+
+            Console.WriteLine("#        #");
+
+            Console.SetCursorPosition(Console.WindowWidth / 2 - 5, ++i);
+            
+            Console.WriteLine("----------");
+
+            Console.SetCursorPosition(Console.WindowWidth / 2 - 5, ++i);
+
+            Console.WriteLine("#   PC   #");
+
+            Console.SetCursorPosition(Console.WindowWidth / 2 - 5, ++i);
+
+            Console.WriteLine($"#   {temp}   #");
+
+            Console.SetCursorPosition(Console.WindowWidth / 2 - 5, ++i);
+
+            Console.WriteLine("#        #");
+
+            Console.SetCursorPosition(Console.WindowWidth / 2 - 5, ++i);
+
+            Console.WriteLine("##########");
+
+        }
+    }
+
+}
