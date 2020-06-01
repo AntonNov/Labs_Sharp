b47ec9a639eb2d4117a911eeeb2c510ccc2bf356
AntonNov <56130014+AntonNov@users.noreply.github.com>
2020-06-01 04:10:37 +0300

    Add files via upload

---
+++ Lab2/Lab2_2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Lab2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите  язык ( ru, en, fr, de): ");
+            string cultureSymbol = Console.ReadLine();
+            try
+            {
+                CultureInfo culture = new CultureInfo(cultureSymbol);
+                if (!CultureInfo.GetCultures(CultureTypes.AllCultures).Contains(culture))
+                    throw new Exception("Неверный язык");
+                foreach (var month in culture.DateTimeFormat.MonthNames)
+                {
+                    Console.WriteLine(month);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+           
+        }
+    }
+}
+
